f36e45c8241b871d90f08391ce87d1b499e19bc9
Nishan <nishan@Nishans-MacBook-Pro.local>
2021-09-07 15:53:08 +0530

    menu integration

--- components/start.js
+++ components/start.js
@@ -1,34 +1,189 @@
 import React from "react";
-import { Button, Menu, MoonIcon, Divider, useDisclose } from "native-base";
+import {
+  Button,
+  Menu,
+  MoonIcon,
+  VStack,
+  Pressable,
+  useDisclose,
+  Box,
+  Text,
+  ChevronRightIcon,
+  HStack,
+} from "native-base";
 
 export const Start = ({ onPress, props }) => {
-	const { isOpen, onToggle } = useDisclose();
-	return (
-		<Button onPress={onPress ?? onToggle} startIcon={<MoonIcon size="xs" />}>
-			Start
-		</Button>
-	);
+  return <StartWithMenu />;
 };
 
 export const StartWithMenu = () => {
-	return (
-		<Menu
-			placement="top left"
-			// placement="top"
-			trigger={(triggerProps) => {
-				return (
-					<Button startIcon={<MoonIcon size="xs" />} {...triggerProps}>
-						Start
-					</Button>
-				);
-			}}
-		>
-			<Menu.Item>Arial</Menu.Item>
-			<Menu.Item>Nunito Sans</Menu.Item>
-			<Menu.Item isDisabled>Tahoma</Menu.Item>
-			<Divider />
-			<Menu.Item>Roboto</Menu.Item>
-			<Menu.Item>Montserrat</Menu.Item>
-		</Menu>
-	);
+  const { isOpen, onToggle } = useDisclose();
+
+  return (
+    <Menu
+      isOpen={isOpen}
+      onClose={onToggle}
+      trigger={(triggerProps) => {
+        return (
+          <Button
+            startIcon={<MoonIcon size="xs" />}
+            {...triggerProps}
+            onPress={onToggle}
+          >
+            Start
+          </Button>
+        );
+      }}
+    >
+      {/* <Menu.Item>Arial</Menu.Item>
+      <Menu.Item>Nunito Sans</Menu.Item>
+      <Menu.Item isDisabled>Tahoma</Menu.Item>
+      <Divider />
+      <Menu.Item>Roboto</Menu.Item>
+      <Menu.Item>Montserrat</Menu.Item> */}
+      <HStack>
+        <Box
+          zIndex={9}
+          alignItems="flex-start"
+          justifyContent="flex-end"
+          bg="win95Secondary"
+          w={8}
+          position="relative"
+        >
+          <Text
+            bottom={0}
+            left={0}
+            position="absolute"
+            fontSize="lg"
+            fontWeight={900}
+            style={{
+              transform: [
+                { rotate: "-90deg" },
+                { translateX: 50 },
+                { translateY: -35 },
+              ],
+            }}
+          >
+            Windows95
+          </Text>
+        </Box>
+        <VStack bg="win95Main">
+          <Pressable
+            px={1}
+            py={2}
+            w="100%"
+            _hover={{ bg: "win95Title.0" }}
+            _pressed={{ bg: "win95Title.0" }}
+          >
+            {({ isHovered, isPressed }) => (
+              <HStack space={2} w="100%" alignItems="center">
+                <MoonIcon color="yellow.500" size="md" />
+                <Text color={isHovered && "white"}>Progrmams</Text>
+                <ChevronRightIcon
+                  size="xs"
+                  ml={10}
+                  color={isHovered && "white"}
+                />
+              </HStack>
+            )}
+          </Pressable>
+          <Pressable
+            px={1}
+            py={2}
+            w="100%"
+            _hover={{ bg: "win95Title.0" }}
+            _pressed={{ bg: "win95Title.0" }}
+          >
+            {({ isHovered, isPressed }) => (
+              <HStack space={2} w="100%" alignItems="center">
+                <MoonIcon color="yellow.500" size="md" />
+                <Text color={isHovered && "white"}>Progrmams</Text>
+                <ChevronRightIcon
+                  size="xs"
+                  ml={10}
+                  color={isHovered && "white"}
+                />
+              </HStack>
+            )}
+          </Pressable>
+          <Pressable
+            px={1}
+            py={2}
+            w="100%"
+            _hover={{ bg: "win95Title.0" }}
+            _pressed={{ bg: "win95Title.0" }}
+          >
+            {({ isHovered, isPressed }) => (
+              <HStack space={2} w="100%" alignItems="center">
+                <MoonIcon color="yellow.500" size="md" />
+                <Text color={isHovered && "white"}>Progrmams</Text>
+                <ChevronRightIcon
+                  size="xs"
+                  ml={10}
+                  color={isHovered && "white"}
+                />
+              </HStack>
+            )}
+          </Pressable>
+          <Pressable
+            px={1}
+            py={2}
+            w="100%"
+            _hover={{ bg: "win95Title.0" }}
+            _pressed={{ bg: "win95Title.0" }}
+          >
+            {({ isHovered, isPressed }) => (
+              <HStack space={2} w="100%" alignItems="center">
+                <MoonIcon color="yellow.500" size="md" />
+                <Text color={isHovered && "white"}>Progrmams</Text>
+                <ChevronRightIcon
+                  size="xs"
+                  ml={10}
+                  color={isHovered && "white"}
+                />
+              </HStack>
+            )}
+          </Pressable>
+          <Pressable
+            px={1}
+            py={2}
+            w="100%"
+            _hover={{ bg: "win95Title.0" }}
+            _pressed={{ bg: "win95Title.0" }}
+          >
+            {({ isHovered, isPressed }) => (
+              <HStack space={2} w="100%" alignItems="center">
+                <MoonIcon color="yellow.500" size="md" />
+                <Text color={isHovered && "white"}>Progrmams</Text>
+                <ChevronRightIcon
+                  size="xs"
+                  ml={10}
+                  color={isHovered && "white"}
+                />
+              </HStack>
+            )}
+          </Pressable>
+          <Pressable
+            px={1}
+            py={2}
+            w="100%"
+            _hover={{ bg: "win95Title.0" }}
+            _pressed={{ bg: "win95Title.0" }}
+          >
+            {({ isHovered, isPressed }) => (
+              <HStack space={2} w="100%" alignItems="center">
+                <MoonIcon color="yellow.500" size="md" />
+                <Text color={isHovered && "white"}>Progrmams</Text>
+                <ChevronRightIcon
+                  size="xs"
+                  ml={10}
+                  color={isHovered && "white"}
+                />
+              </HStack>
+            )}
+          </Pressable>
+        </VStack>
+      </HStack>
+    </Menu>
+  );
 };
