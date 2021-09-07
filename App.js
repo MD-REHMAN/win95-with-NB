d280bad581c6b17d1d7eb7fe319e2611daf4a6ec
MD-REHMAN <rehman.2468@gmail.com>
2021-09-07 10:24:18 +0530

    added start / bottom bar

--- App.js
+++ App.js
@@ -1,7 +1,18 @@
 import React from 'react';
-import { NativeBaseProvider, Box, theme } from 'native-base';
-
-import { Menu, Divider, HamburgerIcon, Pressable } from 'native-base';
+import {
+	NativeBaseProvider,
+	Box,
+	theme,
+	MoonIcon,
+	Menu,
+	Divider,
+	HamburgerIcon,
+	VStack,
+	HStack,
+	Button,
+	Pressable,
+} from 'native-base';
+import { StatusBar } from 'react-native';
 
 export function Example() {
 	return (
@@ -37,6 +48,27 @@ theme.components.ModalFooter = {};
 theme.components.ModalOverlay = {};
 theme.components.Menu = {};
 theme.components.MenuItem = {};
+theme.components.Button.baseStyle = {
+	p: 1,
+	justifyContent: 'center',
+	alignItems: 'center',
+	borderWidth: 2,
+	borderTopColor: '#F2F2F2',
+	borderLeftColor: '#F2F2F2',
+	_stack: {
+		// space: 1,
+		alignItems: 'center',
+	},
+	_text: { fontWeight: 700 },
+	_pressed: {
+		bg: '#EEE',
+		borderTopColor: 'black',
+		borderLeftColor: 'black',
+		borderBottomColor: '#F2F2F2',
+		borderRightColor: '#F2F2F2',
+	},
+};
+theme.components.Button.defaultProps = {};
 
 theme.colors = {
 	...theme.colors,
@@ -46,12 +78,33 @@ theme.colors = {
 	win95TitleDisabled: { 0: '#7B7B7B', 100: '#ADADAD' },
 };
 
+const RootWindow = () => {
+	return (
+		<VStack bg="black" flex={1} safeArea>
+			<Box flex={1} bg="#018281"></Box>
+			<HStack
+				space={2}
+				bg="#C3C3C3"
+				borderTopWidth={2}
+				p={1}
+				borderColor="#F2F2F2"
+			>
+				<Button startIcon={<MoonIcon size="xs" />}> Start</Button>
+				{/* <Divider /> */}
+				<Button pr={8}>Welcome</Button>
+			</HStack>
+		</VStack>
+	);
+};
+
 export default function App() {
 	return (
 		<NativeBaseProvider theme={theme}>
-			<Box flex={1} bg="win95Main" p={20} safeArea>
+			<StatusBar barStyle="light-content"></StatusBar>
+			{/* <Box flex={1} bg="win95Main" p={20} safeArea>
 				Hello world <Example />
-			</Box>
+			</Box> */}
+			<RootWindow />
 		</NativeBaseProvider>
 	);
 }
