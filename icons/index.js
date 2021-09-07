41bf6e888cefed9924da8601c4e4752371008072
MD-REHMAN <rehman.2468@gmail.com>
2021-09-07 18:08:40 +0530

    deleted unused icon

--- icons/index.js
+++ icons/index.js
@@ -31,34 +31,3 @@ export const StartIcon = (props) => {
 		</Svg>
 	);
 };
-
-export const Documents = (props) => {
-	return (
-		<Svg
-			width={32}
-			height={32}
-			fill="none"
-			xmlns="http://www.w3.org/2000/svg"
-			xmlnsXlink="http://www.w3.org/1999/xlink"
-			{...props}
-		>
-			<Path fill="url(#prefix__pattern0)" d="M0 0h32v32H0z" />
-			<Defs>
-				<Pattern
-					id="prefix__pattern0"
-					patternContentUnits="objectBoundingBox"
-					width={1}
-					height={1}
-				>
-					<Use xlinkHref="#prefix__image0" transform="scale(.03125)" />
-				</Pattern>
-				<Image
-					id="prefix__image0"
-					width={32}
-					height={32}
-					xlinkHref="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAMAAABEpIrGAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAQlBMVEUAAACGhoatqZBVVVX/+/DAwMBNTU2AgAD//5nMzGaZmQAAM5mgoKTn59YAMzPMzDMiIiIAAAD/zJkICAgzMwD///8JhSsRAAAAAXRSTlMAQObYZgAAAAFiS0dEFeXY+aMAAAAHdElNRQfiBhgXAzXpQrjsAAAA6ElEQVQ4y5WTgRZDMAxFmZa22ObN/3/rkqaVWrHt6XFw70lSjqb5N+03fuuuuTG2u+Zk9DUYJJEfGYPj+MRrg7j3PkQ+itF9CMqnXINblkLms5Qwt9774b4JD+VT7vJUY3ChnSmRx8V3ti+E1lLGSReFDBHCSdhgIZjjODaWKLgc8OIjXvpkFILbqyTM9iUC8kPntERVARmmO+qxNNsQBcrXCMuaBBSATjIpENa1rgDdC3wh6HhQBSJID+iM2kQF3YVWAA+RBGyvArsxw65FgolCtiGfO5TldROFgJNswmmS0Kxn+eVvfwOSuRzkl7Eh+AAAACV0RVh0ZGF0ZTpjcmVhdGUAMjAxOC0wNi0yNFQyMzowMzo1My0wNDowMMlL3U8AAAAldEVYdGRhdGU6bW9kaWZ5ADIwMTgtMDYtMjRUMjM6MDM6NTMtMDQ6MDC4FmXzAAAAAElFTkSuQmCC"
-				/>
-			</Defs>
-		</Svg>
-	);
-};
